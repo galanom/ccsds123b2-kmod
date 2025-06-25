ecce4a7b9e2fdfb4be6cec2cfd78d43c3bed42ce
Ioannis Galanommatis <galanom@gmail.com>
2025-06-25 16:00:04 +0300

    add simple read/write fops

--- ccsds123b2.c
+++ ccsds123b2.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/io.h>
 #include <linux/debugfs.h>
 #include <linux/uaccess.h>
 
@@ -14,13 +15,47 @@ void *ipcore_virt_addr;
 
 static struct dentry *dbg_dir, *dbg_cfg_dir, *dbg_stats_dir, *parent_dir, *tmp_dir;
 
-static const struct file_operations fops = {
+static ssize_t dbg_read(struct file *file, char __user *ubuf, size_t count, loff_t *ppos)
+{
+	char kbuf[16];
+	u32 reg_off = *(u32 *)file->private_data;
+	if (*ppos > 0)
+		return 0;
+	u32 val = ioread32(ipcore_virt_addr + reg_off);
+	pr_info("Reading register %u returned 0x%x\n", reg_off, val);
+	snprintf(kbuf, 16, "%u\n", val);
+	return simple_read_from_buffer(ubuf, count, ppos, kbuf, sizeof(kbuf) - 1);
+}
+
+static ssize_t dbg_write(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos)
+{
+	char kbuf[16];
+	u32 reg_off = *(u32 *)file->private_data;
+	if (count >= sizeof(kbuf) - 1)
+		return -EINVAL;
+	if (copy_from_user(kbuf, ubuf, count))
+		return -EFAULT;
+	kbuf[count] = '\0';
+	pr_info("read from reg %d string %s\n", reg_off, kbuf);
+	return count;
+}
+
+static const struct file_operations fops_ro = {
 	.owner = THIS_MODULE,
+	.read = dbg_read,
+};
+
+static const struct file_operations fops_rw = {
+	.owner = THIS_MODULE,
+	.read = dbg_read,
+	.write = dbg_write,
 };
 
 static int __init ccsds123b2_init(void)
 {
 	// Generate debugfs entries
+	const struct file_operations *fops;
+
 	dbg_dir = debugfs_create_dir(MODULE_NAME, NULL);
 	if (!dbg_dir)
 		goto err_debugfs;
@@ -32,18 +67,23 @@ static int __init ccsds123b2_init(void)
 		goto err_debugfs;
 
 	for (int i = 0; i < sizeof(entry)/sizeof(entry[0]); ++i) {
-		if (entry[i].value < 32)
+		if (entry[i].value < 32) {
+			fops = &fops_rw;
 			parent_dir = dbg_dir;
-		else if (entry[i].value >= 256)
-			parent_dir = dbg_stats_dir;
-		else
+		} else if (entry[i].value < 256) {
+			fops = &fops_rw;
 			parent_dir = dbg_cfg_dir;
+		} else {
+			fops = &fops_ro;
+			parent_dir = dbg_stats_dir;
+		}
+
 		tmp_dir = debugfs_create_file(
 			entry[i].name,
 			0444,
 			parent_dir,
 			&entry[i].value,
-			&fops);
+			fops);
 		if (!tmp_dir)
 			goto err_debugfs;
 	}
